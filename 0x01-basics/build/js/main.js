8304c7605e5b410046f3ada0aec3687da512be77
Cleve-codes <clevemomanyi@gmail.com>
2024-01-02 22:05:29 +0300

    types

--- 0x01-basics/build/js/main.js
+++ 0x01-basics/build/js/main.js
@@ -1,8 +1,26 @@
 "use strict";
-let userName = "Cleve";
-console.log(userName);
-let a = 12;
-let b = 6;
-let c = 17;
-console.log(a / b);
-console.log(c * b);
+// Lesson 1
+// let userName = "Cleve";
+// console.log(userName);
+// let a: number = 12;
+// let b: number = 6;
+// let c: number = 17;
+// console.log(a / b);
+// console.log(c * b);
+// Lesson 2 - Types (string, number, boolean, any, union)
+let myName = "Cleve";
+let myAge;
+let canVote;
+let albums;
+myName = "Isaac";
+myAge = 20;
+canVote = true;
+albums = 34;
+const sum = (a, b) => {
+    return a + b;
+};
+let postID;
+let isActive;
+let regExp = /\w+/g;
+// myName = 89 // Error
+// myName = 'Isaac' // No Error
--- 0x01-basics/src/main.ts
+++ 0x01-basics/src/main.ts
@@ -1,9 +1,33 @@
-let userName = "Cleve";
-console.log(userName);
+// Lesson 1
+// let userName = "Cleve";
+// console.log(userName);
 
-let a: number = 12;
-let b: number = 6;
-let c: number = 17;
+// let a: number = 12;
+// let b: number = 6;
+// let c: number = 17;
 
-console.log(a / b);
-console.log(c * b);
+// console.log(a / b);
+// console.log(c * b);
+
+// Lesson 2 - Types (string, number, boolean, any, union)
+let myName: string = "Cleve";
+let myAge: number;
+let canVote: boolean;
+let albums: string | number;
+
+myName = "Isaac";
+myAge = 20;
+canVote = true;
+albums = 34;
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+let postID: string | number;
+let isActive: number | boolean | string;
+
+let regExp: RegExp = /\w+/g;
+
+// myName = 89 // Error
+// myName = 'Isaac' // No Error
